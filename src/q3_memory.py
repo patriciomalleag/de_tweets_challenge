f3aa66d97a077c096e88d1e128436e8c76621a2a
Patricio Mallea <patriciomallea21@gmail.com>
2025-06-05 14:49:31 -0400

    .py solutions + ipynb

--- src/q3_memory.py
+++ src/q3_memory.py
@@ -1,4 +1,6 @@
 from typing import List, Tuple
 
+from backend.ijson_backend import top_mentioned_users
+
 def q3_memory(file_path: str) -> List[Tuple[str, int]]:
-    pass
+    return top_mentioned_users(file_path, n=10)
--- src/q3_time.py
+++ src/q3_time.py
@@ -1,4 +1,6 @@
 from typing import List, Tuple
 
+from backend.polars_backend import top_mentioned_users
+
 def q3_time(file_path: str) -> List[Tuple[str, int]]:
-    pass
+    return top_mentioned_users(file_path, n=10)
